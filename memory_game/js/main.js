c6257cd3d6763c78c7d21900b38ae6f4627b27b8
Aaron <aaron.smith@uconn.edu>
2019-06-21 13:03:46 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,21 +21,33 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
+  		alert("You found a match!");
 	} else {
   		alert("Sorry, try again.");
-	}
+	};
 };
-function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID].rank)
-	console.log(cards[cardID].cardImage)
-	console.log(cards[cardID].suit)
-	cardsInPlay.push(cards[cardID].rank)
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	//console.log("User flipped " + cards[cardID].rank)
+	//console.log(cards[cardID].cardImage)
+	//console.log(cards[cardID].suit)
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	}
+	};
 };
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+createBoard();
